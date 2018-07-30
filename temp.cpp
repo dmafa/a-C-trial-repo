27fea67c98acafee4bc07a456c739cabf9c17a04
dmafa <41922005+dmafa@users.noreply.github.com>
2018-07-30 21:06:54 +0300

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+// #include <iomanip> 
+// #include <math.h>
+
+using namespace std;
+
+int main() {
+
+for ( double v = 1 ; v < 8 ; v++ ) 
+   if ( 15 / ( 8+v ) + 15 / (8-v) == 4 ) cout << v << endl ;
+
+
+/*
+cout << endl << 'Z' - 'A' + 1 << endl ;
+
+cout << (char)7 ;   // beep
+  */
+   
+ cin.get () ;
+}
+
